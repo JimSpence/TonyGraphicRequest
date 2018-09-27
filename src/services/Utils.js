7cd436af9a67756926eb67479e5dba1093340dbb
ruddoct <43343049+ruddoct@users.noreply.github.com>
2018-09-27 15:49:41 +0100

    Refactor Sort utility

--- src/services/Utils.js
+++ src/services/Utils.js
@@ -18,26 +18,28 @@ export default class Utils {
         return value;
     };
 
-    static sortByKey = (a, b, object, sortKey, descending, dateSort) => {
-        const from = descending ? b : a;
-        const to = descending ? a : b;
+    static sortByKey = (object, sortKey, descending, dateSort) => {
+        return (a, b) => {
+            const from = descending ? b : a;
+            const to = descending ? a : b;
 
-        let comparatorA = object ? object[from][sortKey] : from;
-        let comparatorB = object ? object[to][sortKey] : to;
+            let comparatorA = object ? object[from][sortKey] : from;
+            let comparatorB = object ? object[to][sortKey] : to;
 
-        if (dateSort) {
-           comparatorA = new Date(comparatorA).getTime();
-           comparatorB = new Date(comparatorB).getTime();
+            if (dateSort) {
+                comparatorA = new Date(comparatorA).getTime();
+                comparatorB = new Date(comparatorB).getTime();
+            }
+
+            if (comparatorA < comparatorB) {
+                return -1;
+            }
+
+            if (comparatorA > comparatorB) {
+                return 1;
+            }
+
+            return 0;
         }
-
-        if (comparatorA < comparatorB) {
-            return -1;
-        }
-
-        if (comparatorA > comparatorB) {
-            return 1;
-        }
-
-        return 0;
     };
 };
